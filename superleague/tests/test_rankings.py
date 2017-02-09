ecad31288d55d95d66e894435c3a6224518a833b
Neilen Marais <nmarais@ska.ac.za>
2017-02-09 08:51:09 +0200

    Add test for parse_team_score, fix embarassing bug exposed by test

--- superleague/tests/test_rankings.py
+++ superleague/tests/test_rankings.py
@@ -1,5 +1,18 @@
 import unittest
 
-class test_rankings(unittest.TestCase):
+from superleague import rankings
+
+class test_parse_team_score(unittest.TestCase):
+    VALID_INPUTS = (
+        'Team A 1', 'Otherteam 123',
+        'The Bestest Team on Earth   912345')
+
+    EXPECTED_VALID_OUTPUTS = (
+        ('Team A', 1), ('Otherteam', 123),
+        ('The Bestest Team on Earth', 912345))
+
     def test_parse_team_score(self):
-        pass
+         for input_val, desired_output in zip(
+                 self.VALID_INPUTS, self.EXPECTED_VALID_OUTPUTS):
+             actual_output = rankings.parse_team_score(input_val)
+             self.assertEqual(actual_output, desired_output)
