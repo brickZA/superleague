c38b9381f39130d1fa25956669a783a60955b97b
Neilen Marais <nmarais@ska.ac.za>
2017-02-09 12:35:23 +0200

    Add docstrings, rename variable for clarity

--- superleague/rankings.py
+++ superleague/rankings.py
@@ -6,6 +6,24 @@ from collections import defaultdict
 # TODO check what pep-8 says about close brakets
 
 def parse_team_score(team_score):
+    """Parse a single team score to a team name and integer score
+
+    Parameters
+    ----------
+
+    team_score : str
+        e.g. "Team A 3"
+
+    Returns
+    -------
+
+    (team_name, score)
+    team_name : str
+        e.g. "Team A"
+    score : int
+        e.g. 3
+
+    """
     team_name, score = team_score.rsplit(' ', 1)
     score = int(score)
     team_name = team_name.strip()
@@ -13,6 +31,22 @@ def parse_team_score(team_score):
     return team_name, score
 
 def parse_line(line):
+    """Parse input line containing a match result
+
+    Parameters
+    ----------
+    line: str
+        e.g. "Tarantulas 1, FC Awesome 0\n"
+
+    Returns
+    -------
+    ((team_a, score_a), (team_b, score_b))
+    team_a/b : str
+        Name of team a/b, e.g. "Tarantuals" or "FC Awesome"
+    score_a/b : int
+        Number of goals scored by team a/b, e.g. 1 or 0.
+
+    """
     team_score1, team_score2 = line.strip().split(',')
     return tuple(
         parse_team_score(tscore.strip())
@@ -21,7 +55,7 @@ def parse_line(line):
 
 
 def calculate_scores(lines):
-    scores = defaultdict(lambda : 0)
+    points = defaultdict(lambda : 0)
     draw_points = 1
     win_points = 3
 
@@ -29,21 +63,22 @@ def calculate_scores(lines):
         if not line:
             continue        # Skip empty lines
 
-        # Get the team names and scores for the current line
+        # Get the team names and points for the current line
         ((team_a, score_a),
          (team_b, score_b)) = parse_line(line)
 
         # Access both teams to ensure that they are initialised to zero in case
         # the team never scores any points
-        scores[team_a]
-        scores[team_b]
+        points[team_a]
+        points[team_b]
 
         if score_a > score_b:
-            scores[team_a] += win_points
+            points[team_a] += win_points
         elif score_b < score_a:
-            scores[team_b] += win_points
+            points[team_b] += win_points
         elif score_a == score_b:
-            scores[team_a] += draw_points
-            scores[team_b] += draw_points
+            points[team_a] += draw_points
+            points[team_b] += draw_points
 
-    return scores
+    return points
+
